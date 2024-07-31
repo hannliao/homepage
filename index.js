2ca3fc81bb4aedbba3b8624fa1e2ebb636d8b1f7
Hannah Liao <128202377+hannliao@users.noreply.github.com>
2024-07-30 22:22:43 -0600

    Add links and fix responsive layout

--- index.js
+++ index.js
@@ -2,55 +2,68 @@ const projects = [
   {
     name: 'Battleship',
     description:
-      'Play a game of battleship against the computer! Still working on drag and drop functionality...',
+      'Play a game of battleship against the computer! Pending updates.',
     src: './img/battleship.png',
+    github: 'https://github.com/hannliao/battleship',
+    demo: 'https://hannliao.github.io/battleship/',
   },
   {
     name: 'Weather App',
     description: 'Search for current weather conditions and 3-day forecast.',
     src: './img/weather-app.png',
+    github: 'https://github.com/hannliao/weather-app',
+    demo: 'https://hannliao.github.io/weather-app/',
   },
   {
     name: 'Sign Up Form',
-    description:
-      'Form validation! Fill out all fields correctly to get a success message.',
+    description: 'Fill out all fields correctly to get a success message.',
     src: './img/signup-form.png',
+    github: 'https://github.com/hannliao/sign-up-form',
+    demo: 'https://hannliao.github.io/sign-up-form/',
   },
   {
     name: 'Library',
     description: 'Catalog books in your own virtual library.',
     src: './img/library.png',
+    github: 'https://github.com/hannliao/library',
+    demo: 'https://hannliao.github.io/library/',
   },
   {
     name: 'Restaurant Page',
-    description:
-      "Restaurant home page has an image carousel that doesn't work on all screen sizes. View menu and contact details as well.",
+    description: 'Tab through home, menu, and contact details.',
     src: './img/restaurant-page.png',
+    github: 'https://github.com/hannliao/restaurant-page',
+    demo: 'https://hannliao.github.io/restaurant-page/',
   },
   {
     name: 'Tic-Tac-Toe',
-    description: 'Play Tic-Tac-Toe with a friend',
+    description: 'Play Tic-Tac-Toe with a friend!',
     src: './img/tictactoe.png',
+    github: 'https://github.com/hannliao/tictactoe',
+    demo: 'https://hannliao.github.io/tictactoe/',
   },
   {
     name: 'To-Do List',
     description:
       'Keep track of your to-dos! Make projects, set deadlines, and prioritize your tasks.',
     src: './img/todo-list.png',
+    github: 'https://github.com/hannliao/todo-list',
+    demo: 'https://hannliao.github.io/todo-list/',
   },
   {
     name: 'Admin Dashboard',
     description:
-      'View snapshots of my older projects! You can find demos on my GitHub.',
+      'View snapshots of my older projects - you can find demos on my GitHub.',
     src: './img/admin-dash.png',
+    github: 'https://github.com/hannliao/admin-dash',
+    demo: 'https://hannliao.github.io/admin-dash/',
   },
 ];
 
-const linkedinIconPath = '/.icons/linkedin.svg';
 const githubIconPath = './icons/github.svg';
 const demoIconPath = './icons/open-in-new.svg';
 
-function addProject(imgSrc, name, description) {
+function addProject(imgSrc, name, description, github, demo) {
   const projectsContainer = document.querySelector('.projects');
   const card = createDiv('card');
 
@@ -64,9 +77,15 @@ function addProject(imgSrc, name, description) {
   const infoHeader = createDiv('card-header');
   const projectName = document.createElement('h2');
   projectName.textContent = name;
+
+  const githubLink = createLink(github);
   const githubIcon = createIcon(githubIconPath);
+  githubLink.appendChild(githubIcon);
+  const demoLink = createLink(demo);
   const demoIcon = createIcon(demoIconPath);
-  infoHeader.append(projectName, githubIcon, demoIcon);
+  demoLink.appendChild(demoIcon);
+
+  infoHeader.append(projectName, githubLink, demoLink);
 
   const projectDesc = document.createElement('p');
   projectDesc.textContent = description;
@@ -82,6 +101,12 @@ function createDiv(className) {
   return div;
 }
 
+function createLink(href) {
+  const link = document.createElement('a');
+  link.href = href;
+  return link;
+}
+
 function createIcon(iconPath) {
   const icon = document.createElement('img');
   icon.src = iconPath;
@@ -90,5 +115,11 @@ function createIcon(iconPath) {
 }
 
 projects.forEach((project) => {
-  addProject(project.src, project.name, project.description);
+  addProject(
+    project.src,
+    project.name,
+    project.description,
+    project.github,
+    project.demo
+  );
 });
